7be9547fb9b8269121af42105c46ad286d8f34d9
rharuyama <aprilfoolsd1y@yahoo.co.jp>
2021-01-04 00:48:11 +0900

    udpate mylang.hs

--- mylang.hs
+++ mylang.hs
@@ -12,10 +12,3 @@ main = do
    src <- readFile (cd ++ "/source")
    (writeFile (cd ++ "/target.s")) . attatchHeadAndTail . codegen . myParser $ src
 
-test :: Either ParseError Exp
-test = parse expr "test" "42+7"
-
-myParser :: String -> Either ParseError Exp
-myParser src = parse expr "mylang" src
-
-
